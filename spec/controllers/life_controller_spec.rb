15f69fabb72841f1d69034bfb200e4d6efd78fde
Dev <devilal@mirraw.com>
2017-12-10 23:46:36 +0530

    Added spec for lneighbours count

---
+++ spec/controllers/life_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe LifeController, type: :controller, order: :defined do
+  subject { Life.new(12,12) }
+
+  describe '#neighbors_count' do
+    before { subject.load([[2,1], [2,2], [2,3]]) }
+
+    it 'expect return 2 for 2,2' do
+      expect(subject.neighbors_count(2, 2)).to eq(2)
+    end
+
+    it 'expect return 3 for 1,2' do
+      expect(subject.neighbors_count(1, 2)).to eq(3)
+    end
+
+    it 'expect return zero for 0,0' do
+      expect(subject.neighbors_count(0, 0)).to eq(0)
+    end
+  end
+end
